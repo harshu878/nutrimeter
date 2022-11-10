88aad85c8a3b61bd908942509f72be666166a461
Ghanishtkhurana <101393463+Ghanishtkhurana@users.noreply.github.com>
2022-11-10 18:54:34 +0530

    day 3

---
+++ fw18_0047_day-3/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom"
+import {ChakraProvider} from "@chakra-ui/react" 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <ChakraProvider>
+    <App />
+    </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
