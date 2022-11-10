9f3bc133018722a5a1f16b44b1e98475da42225e
madhavi-teegireddy <madhavitigireddi@gmail.com>
2022-11-10 22:29:53 +0530

    homepage-completed

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import {NavLink} from "react-router-dom";
 import NavBar from './NavBar';
-import { Box, Flex, Link, Image,HStack, Icon, Text,Spacer,ButtonGroup,Button,Heading,Grid,GridItem, Highlight} from '@chakra-ui/react';
+import { Box, Flex, Link, Image,HStack, Icon, Text,Spacer,ButtonGroup,SimpleGrid,
+  Button,Heading,Grid,GridItem, Highlight} from '@chakra-ui/react';
 
 const fontSize = { base: "10px", sm: "12px", md: "15px", lg: "18px" };
 const HomePage = () => {
@@ -260,7 +261,7 @@ const HomePage = () => {
 
       {/* section-8 starts */}
       <Flex mt="120px">
-        <Text fontSize={{ base: "25px", md: "30px", lg: "40px" }} color="black.600" m={"auto"} fontWeight="bold">
+        <Text fontSize={{ base: "25px", md: "30px", lg: "37px" }} color="gray.600" m={"auto"} fontWeight="bold">
         Sync with Apps and Devices
         </Text>
         </Flex>
@@ -269,6 +270,177 @@ const HomePage = () => {
         Sync Cronometer with these activity trackers, Apple Health, Google Fit and Samsung Health.
         </Text>
         </Flex>
+        <Flex>
+        <Image m="auto" mt="30px" src="https://cdn1.cronometer.com/2021/landing/devices-p-1080.png"></Image>
+        </Flex>
+        {/* section-8 ends */}
+
+        <Grid templateColumns='repeat(1, 1fr)' mt="40px">
+          <GridItem  h='0.5' w="90%" bg='gray' m="auto" />
+        </Grid>
+
+         {/* section-9 starts */}
+        <Flex mt="100px">
+        <Text fontSize={{ base: "25px", md: "30px", lg: "37px" }} color="gray.600" m={"auto"} fontWeight="bold">
+        Cronometer Reviews
+        </Text>
+        </Flex>
+        <Flex w={{base:"60%", md:"70%", lg:"50%"}} m="auto" textAlign={"center"} mt="15px">
+        <Text fontSize={{ base: "18px", md: "20px", lg: "24px" }} color="black.600" m={"auto"}>
+        Check us out in your app store.
+        </Text>
+        </Flex>
+
+      {/* section-10 of 3 divisions with img */}
+      <Grid w="80%" h="auto" border="1px solid lightgray"
+          borderRadius={"10px"} m="auto" mt="70px"
+        justifyContent={"center"}
+        templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(2, 1fr)" }}
+        gap={6}
+        textAlign="center"
+        alignItems={"center"}
+      >
+        <GridItem margin={"auto"} w="80%" display={"grid"} gap={5}>
+          <Heading  fontSize={27} color="orange.500" textAlign="justify" mt={{base:"0px",md:"-350px",lg:"0px"}}>
+            <Image w="60px" mb="-22px" ml="-12px" src="https://cdn1.cronometer.com/2021/landing/quote-icon.svg"></Image>
+          I wasn't the only one concerned about the unreliability of the macros in both MyFitnessPal and Carb Manager.
+          </Heading>
+        </GridItem>
+
+        <GridItem w="80%" margin={"auto"} display={"grid"} gap={5}>
+          <Text color={"text"} fontSize={20} textAlign="justify">
+          I started Keto in July. MyFitnessPal seemed pretty cool. Spent hours setting
+           it up and entering recipes only to discover the macros were not reliable
+            due to all the contributions by the community. I was so frustrated. Then
+             I found a review of several apps (on the Keto Mojo website) and discovered
+            I wasn't the only one concerned about unreliability of the macros
+               on both MFP and CarbManager (tried that too). Behold Cronometer!
+                I love this app. It draws from nutrition databases with minimal
+                 community contributions so much, much more reliability.
+                  There are many more free features. The user interface
+                   is easy to navigate. I'm very happy with Cronometer.
+          </Text>
+          <Text color={"text"} fontSize={20} as="i">
+          iOS Review
+          September 23, 2019
+          </Text>
+        </GridItem>
+
+        <GridItem w="80%" margin={"auto"} display={"grid"} gap={5} mt={{base:"0px",md:"-500px",lg:"-150px"}}>
+          <Image src="https://cdn1.cronometer.com/2021/landing/ios-icon.svg" w="180px" m="auto"></Image>
+        </GridItem>
+      </Grid>
+      {/* section-10 ends */}
+
+{/* section 11 starts */}
+
+      <Grid w="80%" h="auto" border="1px solid lightgray"
+          borderRadius={"10px"} m="auto" mt="70px"
+        justifyContent={"center"}
+        templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(2, 1fr)" }}
+        gap={6}
+        textAlign="center"
+        alignItems={"center"}
+      >
+        
+
+        <GridItem w="80%" margin={"auto"} display={"grid"} gap={5}>
+          <Text color={"text"} fontSize={20} textAlign="justify">
+          This app is my dream! I've been on the hunt for something that can
+          track my daily nutritional needs and the majority of apps seem to
+          think that only means macros. This tracks EVERYTHING! Getting my 
+          daily vitamins through food is so important to me and this has 
+          taken a HUGE load off of my plate. I bought gold within minutes.
+           This is set up great, easy to use, links with my FitBit.
+            I could go on and on. 
+            Android Review
+            November 23, 2020
+          </Text>
+          <Text color={"text"} fontSize={20} as="i">
+          Android Review<br/>
+          November 23, 2020
+          </Text>
+        </GridItem>
+        <GridItem margin={"auto"} w="80%" display={"grid"} gap={5}>
+          <Heading  fontSize={27} color="orange.500" textAlign="justify" mt={{base:"0px",md:"-210px",lg:"0px"}}>
+            <Image w="60px" mb="-22px" ml="-33px" src="https://cdn1.cronometer.com/2021/landing/quote-icon.svg"></Image>
+            Cronometer will track EVERYTHING! ... It's easy to use and links with my FitBit.          </Heading>
+            <Image src="https://cdn1.cronometer.com/2021/landing/ios-icon.svg" w="180px" m="auto"></Image>
+        </GridItem>
+      </Grid>
+
+{/* section-12 green image */}
+<Flex w="full" bg="gray.200" m="auto">
+      <Grid w="70%" h="auto"
+          borderRadius={"10px"} m="auto" mt="70px"
+        justifyContent={"center"}
+        templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)", lg: "repeat(2, 1fr)" }}
+        textAlign="center"
+        alignItems={"center"}
+      >
+        <GridItem w="80%" m="auto" mt="20px"  display={"grid"}>
+          <Image src="https://cdn1.cronometer.com/2021/landing/crono-pro-graphic.svg"></Image>
+        </GridItem>
+        <GridItem m="auto" w="100%" display={"grid"}>
+        <Image w="260px"  ml="70px" src="/pic/logo.png"></Image>
+          <Heading  fontSize={23} color="gray.600" mt='20px'>
+          Are you a healthcare professional?  </Heading>
+          <Text mt="20px" fontSize={{base:"20px", md:"22px", lg:"22px"}}>Check out our software designed for nutritionists, dietitians,
+             research teams, schools, and hospitals.</Text>
+             <Button bg={"white.500"} colorScheme="white.500"  border="2px solid green" color="green" h="60px" w="300px" m="auto" mt="20px">
+                  <Text fontSize={fontSize} p="25px" >Learn Abou Pro</Text>
+              </Button>
+        </GridItem>
+      </Grid>
+      </Flex>
+
+
+      <Flex mt="100px">
+        <Text fontSize={{ base: "25px", md: "30px", lg: "37px" }} color="gray.600" m={"auto"} fontWeight="bold">
+        We are Canadian
+        </Text>
+        </Flex>
+        <Flex w={{base:"60%", md:"70%", lg:"50%"}} m="auto" textAlign={"center"} mt="15px">
+        <Text fontSize={{ base: "18px", md: "20px", lg: "24px" }} color="black.600" m={"auto"}>
+        Independently owned and operated by people who care about their users' nutrition.
+        </Text>
+        </Flex>
+
+        <Flex w="70%" m="auto" mt="40px">
+          <Image src="https://cdn1.cronometer.com/2021/landing/cronometer-staff-2-p-1080.jpeg"></Image>
+        </Flex>
+        <Text m="auto" mt="30px" w="70%" fontSize={{base:"15px", md:"18px", lg:"20px"}} textAlign="center">
+          Our headquarters are based in the idyllic mountain town of Revelstoke, BC, Canada.
+           We are a team of over 25 professionals brought together by a love of nutrition,
+            skiing, biking and the great outdoors.
+          </Text>
+          <Flex>
+          <Button bg={"white.500"} colorScheme="white.500"  border="2px solid orange" color="orange" h="60px" w="300px" m="auto" mt="20px">
+                  <Text fontSize={fontSize} p="25px" >Learn Abou Pro</Text>
+          </Button>
+          </Flex>
+
+          <Grid templateColumns='repeat(1, 1fr)' mt="40px">
+          <GridItem  h='0.5' w="90%" bg='gray' m="auto" />
+        </Grid>
+
+        <Flex w="70%" m="auto" mt="40px">
+        </Flex>
+        <Text m="auto" mt="30px" w="70%" fontSize={{base:"15px", md:"18px", lg:"20px"}} textAlign="center">
+        Help us spread the good word about nutrition and become an affiliate.
+          </Text>
+          <Flex>
+          <Button bg={"white.500"} colorScheme="white.500"  border="2px solid orange" color="orange" h="60px" w="300px" m="auto" mt="20px">
+                  <Text fontSize={fontSize} p="25px" >Become an Affiliate</Text>
+          </Button>
+          </Flex>
+
+          <Flex h="650px" bg="gray" mt="40px">
+
+          </Flex>
+
+
+  
 
 
     </Box>
